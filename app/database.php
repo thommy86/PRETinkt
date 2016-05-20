863607a25a5f3140f724250f7b6e3f22b38d0387
thommy86 <nee_ger@hotmail.com>
2016-05-20 16:35:36 +0200

    connection

---
+++ app/database.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => '127.0.0.1',
+    'database'  => 'pretinktDB',
+    'username'  => 'root',
+    'password'  => 'root',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => ''
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
