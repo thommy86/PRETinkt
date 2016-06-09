cae360b0866f4b0895960317c92093bd52e258ec
Patrick <patrick.devree@gmail.com>
2016-06-09 20:29:02 +0200

    Fix wrong calculation of price

--- app/Helpers/ValutaHelper.php
+++ app/Helpers/ValutaHelper.php
@@ -4,11 +4,11 @@ namespace Webshop\Helpers;
 
 class ValutaHelper {
     public static function CalculatePrice($btw, $price) {
-        return ValutaHelper::RoundValue($price + ($price * $btw));
+        return ValutaHelper::RoundValue($price + ($price * ($btw / 100)));
     }
     
     public static function CalculatePriceQuantity($btw, $price, $quantity) {
-        return ValutaHelper::RoundValue(($price + ($price * $btw)) * $quantity);
+        return ValutaHelper::RoundValue(($price + ($price * ($btw / 100))) * $quantity);
     }
     
     public static function RoundValue($value) {
